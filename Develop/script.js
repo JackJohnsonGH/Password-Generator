f4504a6bdf1ce073812c03cba7d0153576aa6e8b
Jack Johnson <jackjohnson.mtn@gmail.com>
2022-03-03 15:06:20 -0600

    added tools to create character pool

--- Develop/script.js
+++ Develop/script.js
@@ -32,21 +32,54 @@ if (isNaN(passwordLength)) {
   window.alert("Must be a number. Try again!");
   generatePassword();
 }
+
 //makes sure the user provided a valid length
 if (passwordLength > 128 || passwordLength < 8) {
   window.alert("Length must be between 8 and 128. Try again!");
   generatePassword();
 }
 
+//we have a defined variable for the length
 
 //ask the user for password criteria
+//are lowercase characters going to be included?
+var lowercaseCharsCriteria = window.confirm("Do you want to include lowercase letters?");
+//are uppercase characters going to be included?
+var uppercaseCharsCriteria = window.confirm("Do you want to include uppercase letters?");
+//are numeric characters going to be included?
+var numericCharsCriteria = window.confirm("Do you want to include numbers?");
+//are special characters going to be included?
+var specialCharsCriteria = window.confirm("Do you want to include special symbols?")
 //make sure we have at least one criteria
+if ((lowercaseCharsCriteria === false) && (uppercaseCharsCriteria === false) && (numericCharsCriteria === false) && (specialCharsCriteria === false)) {
+  window.alert("Please choose at least one password criteria.");
+  generatePassword();
+}
 
 //random select from our pool of characters to generate the password
+
+//assemble the pool of characters for this pasword
+var characterPool = "";
+
+//what did the user respond for lowercase characters?
+if (lowercaseCharsCriteria === true) {
+  characterPool = characterPool + lowercaseChars
+}
+//what did the user respond for lowercase characters?
+if (uppercaseCharsCriteria === true) {
+  characterPool = characterPool + uppercaseChars
+}
+//what did the user respond for numeric characters?
+if (numericCharsCriteria === true) {
+  characterPool = characterPool + numericChars
+}
+//what did the user respond for special characters?
+if (specialCharsCriteria === true) {
+  characterPool = characterPool + specialChars
+}
+//randomly generate the password from that pool
 
 }
 
 // Non-Function Code
 generateBtn.addEventListener("click", writePassword);
-
-
